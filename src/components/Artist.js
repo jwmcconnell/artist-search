62ad368600ff7cda9d5c91ce5b3eb452457de04a
Jackmc <jackmc1122@gmail.com>
2019-08-21 13:46:07 -0700

    Add some styling to the artists list.

--- src/components/Artist.js
+++ src/components/Artist.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const styles = {
+  item: {
+    border: '1px solid black',
+    textAlign: 'center',
+    width: '300px',
+    height: '200px',
+    margin: '5px',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center'
+  }
+};
+
 const Artist = ({ artist }) => {
   return (
-    <li>
-      <h6>{artist.name}</h6>
+    <li style={styles.item}>
+      <h4>{artist.name}</h4>
+      <p>{artist.disambiguation}</p>
     </li>
   );
 };
--- src/components/Artists.js
+++ src/components/Artists.js
@@ -3,12 +3,21 @@ import PropTypes from 'prop-types';
 
 import Artist from './Artist';
 
+const styles = {
+  list: {
+    listStyle: 'none',
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center'
+  }
+};
+
 const Artists = ({ artistsData }) => {
   const artists = artistsData.map(artist => (
     <Artist key={artist.id} artist={artist} />
   ));
   return (
-    <ul>
+    <ul style={styles.list}>
       {artists}
     </ul>
   );

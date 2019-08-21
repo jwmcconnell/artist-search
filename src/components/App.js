1576fd1d96eccea5c27600b1c9e1feb5e1e87fe9
Jackmc <jackmc1122@gmail.com>
2019-08-21 09:01:21 -0700

    Use router to add the SearchView container

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+
+import SearchView from '../containers/SearchView';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={SearchView} />
+      </Switch>
+    </Router>
+  );
 }
-  
---
+++ src/containers/SearchView.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class SearchView extends React.Component {
+  render() {
+    return <h1>Search View</h1>;
+  }
+}
+
+export default SearchView;

59fdb24e79cf5d6c9ff1c84bb125286ca978cdb1
Jackmc <jackmc1122@gmail.com>
2019-08-22 11:10:58 -0700

    Add test for RecordingList

---
+++ src/components/releaseView/RecordingList.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import RecordingList from './RecordingList';
+
+describe('RecordingList component', () => {
+  it('renders RecordingList', () => {
+    const wrapper = shallow(<RecordingList recordingsData={[]} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
